3e3351b1c625a2d248b6c724a8386bc50c838405
aly <alyhesham2010@live.com>
2018-12-11 22:15:58 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ARCHI_Project
+//
+//  Created by Taymour Abdelal on 12/11/18.
+//  Copyright © 2018 Taymour Abdelal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
